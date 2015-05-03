910eab32abfa6ee177bb8faf152b3c323c07abcf
kennymack <creativeweb.de@gmail.com>
2015-05-03 19:35:34 -0300

    initial commit

---
+++ protractor.js
@@ -0,0 +1,12 @@
+it('should change state', function() {
+  /*var color = element(by.binding('color.name'));
+  var dinamic = element(by.binding('dinamic.name'));
+
+  expect(color.getText()).toContain('blue');
+
+  element.all(by.model('color.name')).get(0).click();*/
+
+  /*element.all(by.model('dinamic.name')).get(0).click();*/
+  
+  /*expect(color.getText()).toContain('red');*/
+});
